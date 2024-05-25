532310f1bf5f70b8d39225b47e12a848cc34028d
chhotelal2310 <chhotupatel705@gmail.com>
2024-05-25 15:35:36 +0530

    update

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-// export const BASE_URL='http://localhost:4000/api/v1'
+ //export const BASE_URL='http://localhost:4000/api/v1'
 
 export const BASE_URL='https://trake-and-travel-kman.onrender.com'
